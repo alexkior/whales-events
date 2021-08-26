c6e2e077dd7890bebd064d073a4ea5fa1b85fdfe
Maxim Yakovlev <it.trainee@yandex.ru>
2021-08-26 10:16:39 +0300

    Release 1 - signUp

--- routes/signUp.js
+++ routes/signUp.js
@@ -10,10 +10,10 @@ router.route('/signUp')
     res.render('signUp')
   })
   .post(async (req, res) => {
-    const { name, email, password } = req.body;
-    if (name && email && password) {
+    const { firstName, lastName, email, password, cityName } = req.body;
+    if (firstName && lastName && email && password && cityName) {
       const hashPass = await bcrypt.hash(password, +process.env.SALTROUND);
-      const newUser = await User.create({ name, email, password: hashPass }, { returning: true, plain: true });
+      const newUser = await User.create({ firstName, lastName, email, password: hashPass, cityName }, { returning: true, plain: true });
       req.session.user = { name: newUser.name, id: newUser.id };
       return res.redirect('/')
     } else {
