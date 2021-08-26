7ecbd119c8946f51229eb04c1f03d1e0d0f31071
Maxim Yakovlev <it.trainee@yandex.ru>
2021-08-26 09:43:17 +0300

    Release 0 - signUp

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
+const { User } = require('../db/models');
+const bcrypt = require('bcrypt');
+
+
+
+router.route('/signUp')
+  .get(async (req, res) => {
+    console.log(req.body)
+    res.render('signUp')
+  })
+  .post(async (req, res) => {
+    const { name, email, password } = req.body;
+    if (name && email && password) {
+      const hashPass = await bcrypt.hash(password, +process.env.SALTROUND);
+      const newUser = await User.create({ name, email, password: hashPass }, { returning: true, plain: true });
+      req.session.user = { name: newUser.name, id: newUser.id };
+      return res.redirect('/')
+    } else {
+      return res.redirect('/user/signUp/?err')
+    }
+  })
+
+
 
 module.exports = router;
