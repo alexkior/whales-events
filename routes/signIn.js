93c6d7a40822c1631736deac8f5d406a97993172
Alex Kior <alex.n.kior@gmail.com>
2021-08-27 17:56:04 +0300

    FINAL RELEASE ATLAST

--- routes/signIn.js
+++ routes/signIn.js
@@ -1,28 +1,31 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const { User } = require("../db/models");
+const { checkUser } = require("../middleware/checkUser");
 
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const { User } = require('../db/models');
-const { checkUser } = require('../middleware/checkUser');
-
-
-router.route('/')
-    .get(async(req, res) => {
-        res.render('signIn');
-    })
-    .post(async(req, res) => {
-        const { email, password } = req.body;
-        console.log(req.body);
-        if (email && password) {
-            const currentUser = await User.findOne({ where: { email } })
-            console.log('addsas');
-            if (currentUser && (await bcrypt.compare(password, currentUser.password))) {
-                req.session.user = { firstName: currentUser.firstName, id: currentUser.id };
-                return res.redirect('/');
-            } else {
-                return res.redirect('/signIn')
-            }
-        }
-    })
-
+router.route("/")
+  .get(async (req, res) => {
+    res.render("signIn");
+  })
+  .post(async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+    if (email && password) {
+      const currentUser = await User.findOne({ where: { email } });
+      console.log("addsas");
+      if (
+        currentUser &&
+        (await bcrypt.compare(password, currentUser.password))
+      ) {
+        req.session.user = {
+          firstName: currentUser.firstName,
+          id: currentUser.id,
+        };
+        return res.redirect("/");
+      } else {
+        return res.redirect("/signIn");
+      }
+    }
+  });
 
 module.exports = router;
